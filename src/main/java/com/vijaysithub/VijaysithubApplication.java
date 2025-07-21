63961070b479de95eab43e5f64123f6b56de74f4
viewvijay22 <50046486+viewvijay22@users.noreply.github.com>
2025-07-21 22:43:06 +0530

    Create VijaysithubApplication.java

---
+++ src/main/java/com/vijaysithub/VijaysithubApplication.java
@@ -0,0 +1,11 @@
+package com.vijaysithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VijaysithubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VijaysithubApplication.class, args);
+    }
+}
